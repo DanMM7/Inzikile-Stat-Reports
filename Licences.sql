b22c20b78b47b802523a98a19930ba4eb1052f10
Dan <dmalengela@yahoo.com>
2023-03-13 11:47:19 +0200

    Added Licences query

---
+++ Licences.sql
@@ -0,0 +1,6 @@
+-- LICENCES --
+-- Approved Building Plans --
+SELECT 'Inspection in ' + MAX(r.PremiseType)  AS 'header', COUNT(*) AS header 
+FROM HealthInspections.dbo.FORM_Premises i 
+INNER JOIN HealthInspections.dbo.FORM_PremisesTypes_LK r on r.PK_ID=i.LK_PremisesTypeID 
+GROUP BY r.PK_ID;
