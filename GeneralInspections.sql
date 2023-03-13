9e582e5af99adeb2c2cf0f450419a5dac66174f0
Dan <dmalengela@yahoo.com>
2023-03-13 11:51:10 +0200

    Added General Inspections query

---
+++ GeneralInspections.sql
@@ -0,0 +1,6 @@
+-- GENERAL INSPECTIONS --
+-- General Inspections per Premises --
+SELECT 'Inspection in ' + MAX(r.PremiseType)  AS 'header', COUNT(*) AS header 
+FROM HealthInspections.dbo.FORM_Premises i 
+INNER JOIN HealthInspections.dbo.FORM_PremisesTypes_LK r on r.PK_ID=i.LK_PremisesTypeID 
+GROUP BY r.PK_ID;
